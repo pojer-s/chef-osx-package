5fb53cdfb758364ee4048210e0cf73eca2389139
pojer_s <steven.pojer@etna-learning.fr>
2014-02-26 15:58:51 +0100

    First commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'osx-package'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures osx-package'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: osx-package
+# Recipe:: default
+#
+# Copyright (C) 2014 ETNA
+# 
+# All rights reserved - Do Not Redistribute
+#
